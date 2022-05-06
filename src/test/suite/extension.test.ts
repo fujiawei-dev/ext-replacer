3ca0db1161340d4efbae1a570f80a8b8b55675f8
Rustle Karl <fu.jiawei@outlook.com>
2022-05-06 16:05:02 +0800

    fix: 半角冒号不插入空格

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -40,6 +40,7 @@ suite('Extension Test Suite', () => {
 		assert.strictEqual("用 write() 函数", replacer.repairRecognizingText("用writeO）函数"));
 		assert.strictEqual("(c) 路径", replacer.repairRecognizingText("（c() 路径"));
 		assert.strictEqual("进程 A 中关闭 sv[0]，在进程 B 中关闭 sv[1]", replacer.repairRecognizingText("进程A中关闭sv【0】，在进程B中关闭sv【1】"));
+		assert.strictEqual("目的 MAC 地址为 00:00:00:00:00:02", replacer.repairRecognizingText("目的MAC地址为00:00:00:00:00:02"));
 
 		assert.strictEqual("repairRecognizingText", replacer.repairRecognizingText("repairRecognizingText"));
 	});
